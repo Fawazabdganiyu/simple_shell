ac84e869ffda4056f66c1ffa22f471f4734d39f3
fawazabdganiyu <fawazabdganiyu@gmail>
2023-07-20 09:00:55 +0100

    Initiate _getline function

---
+++ _getline.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * _getline - reads an entire line from a stream
+ * @lineptr: A pointer to the malloc'ed buffer
+ * @n: The number of bytes read input the buffer
+ * @stream: The standard input stream
+ *
+ * Return: The number of characters read to the buffer
+ */
+size_t *_getline(char **lineptr, size_t *n, FILE *stream)
+{
+	static
+}
+
