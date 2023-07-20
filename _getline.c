32e86fc761a3da273445c8320361faac7a85d938
fawazabdganiyu <fawazabdganiyu@gmail>
2023-07-20 14:01:32 +0100

    Make progress

--- _getline.c
+++ _getline.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+#define LINE_SIZE 1024
+
 /**
  * _getline - reads an entire line from a stream
  * @lineptr: A pointer to the malloc'ed buffer
@@ -7,9 +9,31 @@
  * @stream: The standard input stream
  *
  * Return: The number of characters read to the buffer
+ *	-1 on failure to read a line
  */
 size_t *_getline(char **lineptr, size_t *n, FILE *stream)
 {
-	static
+	static char *lineptr;
+	static size_t index = 0;
+	static size_t nread = 0;
+	char current_char;
+
+	*lineptr = malloc(sizeof(char) * LINE_SIZE);
+	if (!*lineptr)
+		return (NULL);
+
+	while ((index < nread) || (nread = read(STDIN_FILENO, *lineptr, LINE_SIZE) > 0))
+	{
+		while (index < nread)
+		{
+			current_char = *lineptr[index++];
+			if (current_char == '\n')
+			{
+				*lineptr[index - 1] = '\0';
+
+				return;
+			}
+		}
+	}
 }
 
