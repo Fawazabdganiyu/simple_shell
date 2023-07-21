a14115d5c64c6b1a660504d549f41d066ffbe0bd
fawazabdganiyu <fawazabdganiyu@gmail>
2023-07-21 23:23:31 +0100

    Resolve error uninitialised value was created by a heap allocation

--- _getline.c
+++ _getline.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
-#define LINE_SIZE 1024
+#define INITIAL_SIZE 256
+#define ONE_BYTE 1
 
 /**
  * _getline - reads an entire line from a stream
@@ -13,16 +14,47 @@
  */
 ssize_t _getline(char **lineptr, size_t *n, int fd)
 {
-	static size_t nread = 0;
+	size_t nread = 0, c;
+	size_t buf_size = *n;
+	char *new_ptr;
 
-	*lineptr = malloc(sizeof(char) * LINE_SIZE);
-	if (!*lineptr)
-		return (-1);
-
-	if ((nread = read(fd, *lineptr, LINE_SIZE)) > 0)
+	if (*lineptr == NULL || buf_size == 0)
 	{
+		*lineptr = malloc(sizeof(char) * INITIAL_SIZE);
+		if (*lineptr == NULL)
+			return (-1);
+		buf_size = INITIAL_SIZE;
+	}
+
+	while ((c= read(fd, &(*lineptr)[nread++], ONE_BYTE)) > 0)
+	{
+		/* Reallocate if the malloc'ed is not sufficient */
+		if (nread >= buf_size)
+		{
+			buf_size *= 2;
+			new_ptr = realloc(*lineptr, sizeof(char) * buf_size);
+			if (!new_ptr)
+				return (-1);
+
+			*lineptr = new_ptr;
+		}
+
+		/* Detect if a new line is read and initialise */
+		/* the next character '\0' then return */
+		if ((*lineptr)[nread - 1] == '\n')
+		{
+			(*lineptr)[nread] = '\0';
+			*n = nread;
+			return (nread);
+		}
+	}
+
+	/* Return what has been read on EOF */
+	if (nread > 0)
+	{
+		(*lineptr)[nread] = '\0';
 		*n = nread;
-		return (0);
+		return (nread);
 	}
 
 	return (-1);
