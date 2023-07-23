d327ed4ddd618b79e656f1949d587ee53a73e1a9
fawazabdganiyu <fawazabdganiyu@gmail>
2023-07-23 18:23:16 +0100

    Modify cd condition structure

--- chdir.c
+++ chdir.c
@@ -13,17 +13,16 @@ void cd(char **env, char *new_dir)
 	if (new_dir != NULL)
 	{
 		if ((_strcmp(new_dir, "-") == 0))
-		{	if (chdir(old_dir) == -1)
+			if (chdir(old_dir) == -1)
 				perror("Chdir");
-		}
-		else
-		{
+
+		if ((_strcmp(new_dir, "-") != 0))
 			if (chdir(new_dir) == -1)
 				perror("Chdir");
-		}
 	}
-	if (chdir(home) == -1)
-		perror("Chdir");
+	else
+		if ((chdir(home) == -1))
+			perror("Chdir");
 
 	current_dir = getcwd(buf, size);
 	setenv("PWD", current_dir, 1);
