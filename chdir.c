0bb0b86d6f14098b16d73a7a7f4addb160f80b80
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-23 20:29:19 +0100

    Switching branch from cd to main to pull changes

--- chdir.c
+++ chdir.c
@@ -2,8 +2,9 @@
 
 void cd(char **env, char *new_dir)
 {
-	size_t size;
+	/*size_t size, size_2;
 	char *buf = NULL;
+	char *buf_2 = NULL;
 	char *cwd = getcwd(buf, size);
 	char *home = _getenv("HOME");
 	char *old_dir = _getenv("OLDPWD");
@@ -18,12 +19,15 @@ void cd(char **env, char *new_dir)
 		if (_strcmp(new_dir, "-") == 0)
 			if (chdir(old_dir) == -1)
 				perror("Chdir");
-		else
-			if (chdir(home) == -1)
-				perror("Chdir");
 	}
-	current_dir = getcwd(buf, size);
+	else
+		if (chdir(home) == -1)
+			perror("Chdir");
+	current_dir = getcwd(buf_2, size_2);
 	_setenv(env, "PWD", current_dir, 1);
 	_setenv(env, "OLDPWD", cwd, 1);
 	free(buf);
+	free(buf_2);*/
+	_setenv(env, "PWD", "NEW", 1);
+	_setenv(env, "OLDPWD", "OLD", 1);
 }
