9133239d7b22334d1a114cc8fdb7c16fd41f7e51
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-27 23:00:20 +0100

    Handle file not found

--- file_input.c
+++ file_input.c
@@ -28,13 +28,13 @@ int file_input(char **av, char **env, char *buf,
 		return ('x');
 	if (access(file, F_OK) == -1)
 	{
-		_puts(program);
-		_puts(": ");
+		write(STDERR_FILENO, program, _strlen(program));
+		write(STDERR_FILENO, ": ", 2);
 		put_u_int(*m);
-		_puts(": cannot open ");
-		_puts(file);
-		_puts(": No such file\n");
-		return (0);
+		write(STDERR_FILENO, ": cannot open ", 14);
+		write(STDERR_FILENO, file, _strlen(file));
+		write(STDERR_FILENO, ": No such file\n", 15);
+		exit(127);
 	}
 	file_holder = fopen(file, "r");
 	while ((nread = getline(&buffer, &size, file_holder)) > 0)
